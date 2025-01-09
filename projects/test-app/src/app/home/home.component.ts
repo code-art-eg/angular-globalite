75788c766ef8450947ef65ccb458928bf0c86121
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-10 00:24:57 +0200

    add test application

---
+++ projects/test-app/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {AngularGlobaliteComponent} from '@code-art-eg/angular-globalite';
+
+@Component({
+  selector: 'app-home',
+  imports: [AngularGlobaliteComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
