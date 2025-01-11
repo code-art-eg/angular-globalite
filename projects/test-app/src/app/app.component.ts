9018923d65ec6eddf24d2b2d8fbd430b3f7fce98
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-11 14:25:01 +0200

    add angular-bootstrap dependency

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -1,88 +1,19 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-
-export const THEMES = ['light', 'dark', null];
-//export const THEME_ICONS = ['sun', 'moon-stars', 'circle-half'];
-
-const APP_THEME_KEY = 'app-theme';
-const APP_THEME_ATTR = 'data-bs-theme';
+import {
+	ThemePickerComponent,
+	ThemeService,
+} from '@code-art-eg/angular-bootstrap';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, ThemePickerComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-	#document = inject(DOCUMENT);
-	#matcher: MediaQueryList | null = null;
-	readonly #listener: (e: MediaQueryListEvent) => void;
-	readonly #theme = signal<string | null>(this.#currentTheme);
-	//readonly theme = this.#theme.asReadonly();
-	#listenerSetup = false;
-
-	constructor() {
-		this.#listener = this.#themeListener.bind(this);
-		this.#applyTheme();
-	}
+	// eslint-disable-next-line no-unused-private-class-members
+	#themeService = inject(ThemeService);
 
 	title = 'test-app';
-
-	get #currentTheme(): string | null {
-		if (!localStorage) {
-			return null;
-		}
-		const current = localStorage.getItem(APP_THEME_KEY);
-		if (!current || !THEMES.includes(current)) {
-			return null;
-		}
-		return current;
-	}
-
-	set #currentTheme(theme: string | null) {
-		if (!THEMES.includes(theme)) {
-			theme = null;
-		}
-		if (!theme) {
-			localStorage.removeItem(APP_THEME_KEY);
-		} else {
-			localStorage.setItem(APP_THEME_KEY, theme);
-		}
-		this.#applyTheme();
-		this.#theme.set(theme);
-	}
-
-	#applyTheme(): void {
-		if (!this.#document) {
-			return;
-		}
-		let theme = this.#currentTheme;
-		if (!theme) {
-			theme = this.matcher.matches ? 'dark' : 'light';
-			if (!this.#listenerSetup) {
-				this.matcher.addEventListener('change', this.#listener);
-				this.#listenerSetup = true;
-			}
-		} else if (this.#listenerSetup) {
-			if (this.#listenerSetup) {
-				this.matcher.removeEventListener('change', this.#listener);
-				this.#listenerSetup = false;
-			}
-		}
-		this.#document.body.setAttribute(APP_THEME_ATTR, theme);
-	}
-
-	private get matcher(): MediaQueryList {
-		if (!this.#matcher) {
-			this.#matcher = this.#document.defaultView!.matchMedia(
-				'(prefers-color-scheme: dark)'
-			);
-		}
-		return this.#matcher;
-	}
-
-	#themeListener(): void {
-		this.#applyTheme();
-	}
 }
