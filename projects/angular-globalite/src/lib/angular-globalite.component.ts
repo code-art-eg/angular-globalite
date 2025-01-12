ea6d99127728593389356d74e5d8e73e30627e9f
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-12 14:29:03 +0200

    remove temp files and update public api

--- projects/angular-globalite/src/lib/angular-globalite.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'lib-angular-globalite',
-	imports: [],
-	template: ` <p>angular-globalite works!</p> `,
-	styles: ``,
-})
-export class AngularGlobaliteComponent {}
--- projects/angular-globalite/src/public-api.ts
+++ projects/angular-globalite/src/public-api.ts
@@ -2,5 +2,16 @@
  * Public API Surface of angular-globalite
  */
 
-export * from './lib/angular-globalite.service';
-export * from './lib/angular-globalite.component';
+export * from './lib/services/angular-locale-provider.service';
+export * from './lib/services/cookie-locale-provider.service';
+export * from './lib/services/locale.service';
+export * from './lib/services/navigator-language-locale-provider.service';
+export * from './lib/services/storage-locale-provider.service';
+
+export type {
+	LocaleProvider,
+	CookieLocaleConfig,
+	StorageLocaleConfig,
+} from './lib/types';
+
+export * from './lib/pipes/globalize-number.pipe';
