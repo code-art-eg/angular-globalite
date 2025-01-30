6817e7eed418384b304ce316a756942dc978a5d5
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-30 18:11:13 +0200

    add duration example code

--- projects/test-app/src/app/duration-pipe-example/duration-pipe-example.component.ts
+++ projects/test-app/src/app/duration-pipe-example/duration-pipe-example.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GlobalizeDurationPipe } from '@code-art-eg/angular-globalite';
 
 @Component({
 	selector: 'app-duration-pipe-example',
-	imports: [],
+	imports: [GlobalizeDurationPipe],
 	templateUrl: './duration-pipe-example.component.html',
 })
 export class DurationPipeExampleComponent {}
