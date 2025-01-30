5e1ce072ccda5acff57ecf0cfa7661fbd809245a
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-30 18:14:06 +0200

    add jsdocs to gbool pipe

--- projects/angular-globalite/src/lib/pipes/globalize-boolean.pipe.ts
+++ projects/angular-globalite/src/lib/pipes/globalize-boolean.pipe.ts
@@ -2,6 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { BaseGlobalizePipe } from './base-globalize-pipe';
 import { BooleanFormatter, booleanFormatter } from '@code-art-eg/globalite';
 
+/**
+ * Pipe to format boolean values using Globalite.
+ *
+ * @example
+ * {{ true | gbool }} Uses the default boolean format and current locale. shows 'yes' in English, 'ja' in German, etc.
+ * {{ false | gbool }} Uses the default boolean format and current locale. shows 'no' in English, 'nein' in German, etc.
+ * {{ true | gbool:'': 'de' }} Uses the default boolean format and the locale 'de'. (shows 'ja')
+ */
 @Pipe({
 	name: 'gbool',
 	pure: false,
@@ -13,6 +21,14 @@ export class GlobalizeBooleanPipe
 	#booleanFormatter?: BooleanFormatter;
 	#locale?: string;
 
+	/**
+	 * Transforms the input boolean value to a formatted string based on the provided locale.
+	 *
+	 * @param {boolean} input - The boolean value to format.
+	 * @param {string} _ - Unused parameter for options format.
+	 * @param {string} locale - The locale to use for formatting.
+	 * @returns {string} The formatted boolean value.
+	 */
 	protected override transformValue(
 		input: boolean,
 		_: string,
@@ -26,6 +42,11 @@ export class GlobalizeBooleanPipe
 		return this.#booleanFormatter(input);
 	}
 
+	/**
+	 * Gets the default options or format for the boolean pipe.
+	 *
+	 * @returns {string} The default format.
+	 */
 	protected override getDefaultOptionsOrFormat(): string {
 		return '';
 	}
